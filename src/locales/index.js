32e9949c25850c7d652421abc04c83b1a043f621
chenhf <4019980@qq.com>
2017-06-12 13:52:22 +0800

    update

--- src/locales/index.js
+++ src/locales/index.js
@@ -3,11 +3,13 @@ import cookies from 'cookies-js'
 
 import zh_CN from './zh-CN'
 import en_US from './en-US'
+import ja from './ja'
 
 const messageObj = {
     'zh-CN': zh_CN,
     'en-US': en_US,
     'en': en_US,
+    'ja': ja,
 }
 
 export const locale = cookies.get('lang') || BrowserLocale()
